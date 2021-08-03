1c115b7534ea1804330cc4c4860ca36d129ee268
Carlos Augusto Luna da Silva <carlos.luna@meta.com.br>
2021-08-03 18:13:47 -0300

    curso react alura dia 1

---
+++ index.js
@@ -0,0 +1,38 @@
+class Cliente{
+    nome;
+    cpf;
+}
+
+class ContaCorrente{
+    agencia;
+    #saldo = 0; // o hashtag ( # ) nesse caso está setando a propriedade da classe como PRIVATE
+
+    sacar(valor){
+        if(this.#saldo >= valor){
+        this.#saldo -= valor;
+        return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.#saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.#saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(3000);
+contaCorrenteRicardo.sacar(180)
+console.log(contaCorrenteRicardo)
