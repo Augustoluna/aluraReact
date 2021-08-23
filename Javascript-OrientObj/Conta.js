3165417c5490e3e19a38c0538e1dbbaaba1c206c
Carlos Augusto Luna da Silva <carlos.luna@meta.com.br>
2021-08-23 09:21:23 -0300

    estudos de classes e metodos abstratos

--- Javascript-OrientObj/Conta.js
+++ Javascript-OrientObj/Conta.js
@@ -1,45 +1,50 @@
+//Classe abstrata 
 export class Conta {
-  constructor(saldoInicial, cliente, agencia) {
-    this._saldo = saldoInicial;
-    this._cliente = cliente;
-    this._agencia = agencia;
-  }
+    constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstata");
+        }
 
-  set cliente(novoValor) {
-    if (novoValor instanceof Cliente) {
-      this._cliente = novoValor;
-    }
-  }
-
-  get cliente() {
-    return this._cliente;
-  }
-
-  get saldo() {
-    return this._saldo;
-  }
-
-  sacar(valor) {
-    let taxa = 1;
-    return this._sacar(valor, taxa);
-  }
-
-  _sacar(valor, taxa) {
-    const valorSacado = taxa * valor;
-    if (this._saldo >= valorSacado) {
-      this._saldo -= valorSacado;
-      return valorSacado;
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
     }
 
-    return 0;
-  }
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
 
-  depositar(valor) {
-    this._saldo += valor;
-  }
+    get cliente() {
+        return this._cliente;
+    }
 
-  tranferir(valor, conta) {
-    const valorSacado = this.sacar(valor);
-    conta.depositar(valorSacado);
-  }
+    get saldo() {
+        return this._saldo;
+    }
+
+    //Método abstrato
+    sacar(valor) {
+       throw new Error("O método Sacar da conta é abstrato")
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor) {
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
